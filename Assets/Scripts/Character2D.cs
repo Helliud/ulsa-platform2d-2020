a8e4286bdbfde26787047e4057858b0e477a1ada
unknown <Helliud>
2020-01-24 08:07:50 -0700

    Ya sin pedos xd

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Platform2DUtils.GameplaySystem;
 
 public class Character2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected SpriteRenderer spr;
+    protected Animator anim;
+    protected Rigidbody2D rb2D;
+    [SerializeField, Range(1f, 10f)]
+    protected float jumpForce = 7f; 
+   
+    [SerializeField]
+    protected float moveSpeed = 2f;
+
+    //----------------------------------------------------------------------
+    
+    [SerializeField]
+    Color rayColor = Color.magenta;
+
+    [SerializeField, Range(0.1f, 5.0f)]
+    float rayDistance = 5.0f;
+
+    [SerializeField]
+    LayerMask groundLayer;
+
+    //----------------------------------------------------------------------
+
+
+    void Awake()
     {
-        
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected bool FlipSprite
     {
-        
+        get => GameplaySystem.Axis.x < 0 ? true : GameplaySystem.Axis.x > 0 ? false : spr.flipX;
     }
+
+    protected bool Grouding
+    {
+        get => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+    }
+
+    //Drawing raycast
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);
+    }
+
+
+
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platform2DUtils.GameplaySystem;
+
+public class Player : Character2D
+{
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(GameplaySystem.JumpBtn)
+        {
+            if(Grouding)
+            {
+                anim.SetTrigger("jump");
+                GameplaySystem.Jump(rb2D, jumpForce);
+            }
+        }
+
+        anim.SetBool("grounding", Grouding);
+    }
+
+    void Update()
+    {
+        GameplaySystem.TMovementDelta(transform, moveSpeed);
+    }
+
+    void LateUpdate()
+    {
+        spr.flipX = FlipSprite;
+        anim.SetFloat("axisX", Mathf.Abs(GameplaySystem.Axis.x));
+    }
+}
